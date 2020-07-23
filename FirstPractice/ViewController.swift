507bc64298e53b15229f3bf8baa334cd8abc8c7d
Miguel <eduardovaldeztellez@gmail.com>
2020-07-23 09:40:29 -0500

    Initial Commit

---
+++ FirstPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstPractice
+//
+//  Created by Miguel Eduardo  Valdez Tellez  on 23/07/20.
+//  Copyright © 2020 Miguel Eduardo  Valdez Tellez . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
